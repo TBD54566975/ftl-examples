60fc9bac4c3b6bbd7132b96643a0623258143219
Wes <wesbillman@users.noreply.github.com>
2024-02-13 15:07:18 -0700

    Add go and kotlin examples (#20)

---
+++ go/http/http.go
@@ -0,0 +1,108 @@
+//ftl:module http
+package http
+
+import (
+	"context"
+	"fmt"
+
+	"ftl/builtin" // Import the FTL built-in package.
+
+	"github.com/TBD54566975/ftl/go-runtime/ftl" // Import the FTL SDK.
+)
+
+type GetRequest struct {
+	UserID string `alias:"userId"`
+	PostID string `alias:"postId"`
+}
+
+type Nested struct {
+	GoodStuff string `alias:"good_stuff"`
+}
+
+type GetResponse struct {
+	Message string `alias:"msg"`
+	Nested  Nested `alias:"nested"`
+}
+
+type ErrorResponse struct {
+	Error string `alias:"error"`
+}
+
+// Example:       curl -i http://localhost:8892/ingress/http/users/123/posts?postId=456
+// Error Example: curl -i http://localhost:8892/ingress/http/users/000/posts?postId=456
+//
+//ftl:verb
+//ftl:ingress http GET /http/users/{userId}/posts
+func Get(ctx context.Context, req builtin.HttpRequest[GetRequest]) (builtin.HttpResponse[GetResponse, ErrorResponse], error) {
+	if req.Body.UserID == "000" {
+		return builtin.HttpResponse[GetResponse, ErrorResponse]{
+			Error: ftl.Some(ErrorResponse{
+				Error: "User not found",
+			}),
+		}, nil
+	}
+
+	return builtin.HttpResponse[GetResponse, ErrorResponse]{
+		Headers: map[string][]string{"Get": {"Header from FTL"}},
+		Body: ftl.Some(GetResponse{
+			Message: fmt.Sprintf("Got userId %s and postId %s", req.Body.UserID, req.Body.PostID),
+			Nested:  Nested{GoodStuff: "Nested Good Stuff"},
+		}),
+	}, nil
+}
+
+type PostRequest struct {
+	UserID int `alias:"user_id"`
+	PostID int `alias:"post_id"`
+}
+
+type PostResponse struct {
+	Success bool `alias:"success"`
+}
+
+// Example: curl -i --json '{"user_id": 123, "post_id": 345}' http://localhost:8892/ingress/http/users
+//
+//ftl:verb
+//ftl:ingress http POST /http/users
+func Post(ctx context.Context, req builtin.HttpRequest[PostRequest]) (builtin.HttpResponse[PostResponse, ftl.Unit], error) {
+	return builtin.HttpResponse[PostResponse, ftl.Unit]{
+		Status:  201,
+		Headers: map[string][]string{"Post": {"Header from FTL"}},
+		Body:    ftl.Some(PostResponse{Success: true}),
+	}, nil
+}
+
+type PutRequest struct {
+	UserID string `alias:"userId"`
+	PostID string `alias:"postId"`
+}
+
+type PutResponse struct{}
+
+// Example: curl -X PUT http://localhost:8892/ingress/http/users/123 -d '{"postId": "123"}'
+//
+//ftl:verb
+//ftl:ingress http PUT /http/users/{userId}
+func Put(ctx context.Context, req builtin.HttpRequest[PutRequest]) (builtin.HttpResponse[PutResponse, ftl.Unit], error) {
+	return builtin.HttpResponse[PutResponse, ftl.Unit]{
+		Headers: map[string][]string{"Put": {"Header from FTL"}},
+		Body:    ftl.Some(PutResponse{}),
+	}, nil
+}
+
+type DeleteRequest struct {
+	UserID string `alias:"userId"`
+}
+
+type DeleteResponse struct{}
+
+// Example: curl -X DELETE http://localhost:8892/ingress/http/users/123
+//
+//ftl:verb
+//ftl:ingress http DELETE /http/users/{userId}
+func Delete(ctx context.Context, req builtin.HttpRequest[DeleteRequest]) (builtin.HttpResponse[DeleteResponse, ftl.Unit], error) {
+	return builtin.HttpResponse[DeleteResponse, ftl.Unit]{
+		Headers: map[string][]string{"Put": {"Header from FTL"}},
+		Body:    ftl.Some(DeleteResponse{}),
+	}, nil
+}
---
+++ kotlin/ftl-module-http/src/main/kotlin/ftl/http/Http.kt
@@ -0,0 +1,91 @@
+package ftl.http
+
+import ftl.builtin.Empty
+import ftl.builtin.HttpRequest
+import ftl.builtin.HttpResponse
+import kotlin.String
+import xyz.block.ftl.Alias
+import xyz.block.ftl.Context
+import xyz.block.ftl.HttpIngress
+import xyz.block.ftl.Method
+import xyz.block.ftl.Verb
+
+data class GetRequest(
+    @Alias("userId") val userID: String,
+    @Alias("postId") val postID: String,
+)
+
+data class Nested(
+    @Alias("good_stuff") val goodStuff: String,
+)
+
+data class GetResponse(
+    @Alias("msg") val message: String,
+    @Alias("nested") val nested: Nested,
+)
+
+data class PostRequest(
+    @Alias("user_id") val userId: Int,
+    @Alias("post_id") val postId: Int,
+)
+
+data class PostResponse(
+    @Alias("success") val success: Boolean,
+)
+
+data class PutRequest(
+    @Alias("userId") val userID: String,
+    @Alias("postId") val postID: String,
+)
+
+data class DeleteRequest(
+    @Alias("userId") val userID: String,
+)
+
+// Example:       curl -i http://localhost:8892/ingress/http/users/123/posts?postId=456
+// Error Example: curl -i http://localhost:8892/ingress/http/users/000/posts?postId=456
+@Verb
+@HttpIngress(Method.GET, "/http/users/{userId}/posts")
+fun `get`(context: Context, req: HttpRequest<GetRequest>): HttpResponse<GetResponse, String> {
+    return HttpResponse(
+        status = 200,
+        headers = mapOf("Get" to arrayListOf("Header from FTL")),
+        body = GetResponse(
+            message = "UserID: ${req.body.userID}, PostID: ${req.body.postID}",
+            nested = Nested(goodStuff = "This is good stuff")
+        )
+    )
+}
+
+// Example: curl -i --json '{"user_id": 123, "post_id": 345}' http://localhost:8892/ingress/http/users
+@Verb
+@HttpIngress(Method.POST, "/http/users")
+fun post(context: Context, req: HttpRequest<PostRequest>): HttpResponse<PostResponse, String> {
+    return HttpResponse(
+        status = 201,
+        headers = mapOf("Post" to arrayListOf("Header from FTL")),
+        body = PostResponse(success = true)
+    )
+}
+
+// Example: curl -X PUT http://localhost:8892/ingress/http/users/123 -d '{"postId": "123"}'
+@Verb
+@HttpIngress(Method.PUT, "/http/users/{userId}")
+fun put(context: Context, req: HttpRequest<PutRequest>): HttpResponse<Empty, String> {
+    return HttpResponse(
+        status = 200,
+        headers = mapOf("Put" to arrayListOf("Header from FTL")),
+        body = Empty()
+    )
+}
+
+// Example: curl -X DELETE http://localhost:8892/ingress/http/users/123
+@Verb
+@HttpIngress(Method.DELETE, "/http/users/{userId}")
+fun delete(context: Context, req: HttpRequest<DeleteRequest>): HttpResponse<Empty, String> {
+    return HttpResponse(
+        status = 200,
+        headers = mapOf("Delete" to arrayListOf("Header from FTL")),
+        body = Empty()
+    )
+}
