b7e2df15a7fa9dfacb61a3ff2dca81a504c003af
Matt Toohey <mtoohey@block.xyz>
2024-04-16 14:12:10 +1000

    feat: add cronjobs go module

---
+++ go/cronjobs/cronjobs.go
@@ -0,0 +1,34 @@
+//ftl:module cronjobs
+package cronjobs
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+//ftl:cron */10 * * * * * *
+func EveryTenSeconds(ctx context.Context) error {
+	fmt.Printf("Every ten seconds: %d\n", time.Now().Second())
+	return nil
+}
+
+//ftl:cron 0 * * * * * *
+func EveryMinute(ctx context.Context) error {
+	fmt.Printf("Every minute %v\n", time.Now().Minute())
+	return nil
+}
+
+//ftl:cron 0 0 0 * * * *
+func EveryDay(ctx context.Context) error {
+	fmt.Printf("Every day\n")
+	return nil
+}
+
+//ftl:cron */3 * * * * * *
+func LongAndFrequentCronJob(ctx context.Context) error {
+	fmt.Printf("Long and frequent: Started\n")
+	time.Sleep(time.Second * 30)
+	fmt.Printf("Long and frequent: Ended\n")
+	return nil
+}
